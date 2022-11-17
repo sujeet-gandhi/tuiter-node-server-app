9cc18c8e4d381cda756b391feb9b152762be5c50
sujeet-gandhi <sujeetgandhi014@gmail.com>
2022-11-17 00:25:28 -0800

    modify data

--- src/services/tuits-thunks.js
+++ src/services/tuits-thunks.js
@@ -1,7 +1,7 @@
 import {createAsyncThunk}
     from "@reduxjs/toolkit"
 import * as service
-    from "./tuits-service"
+    from "./tuiter-services.js"
 
 export const findTuitsThunk = createAsyncThunk(
     'tuits/findTuits', async () =>
