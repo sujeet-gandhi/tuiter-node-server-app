b82027c66bbe835bbf86c51435dfc56075b631d4
sujeet-gandhi <sujeetgandhi014@gmail.com>
2022-11-18 19:20:50 -0800

    add dislike count

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -4,15 +4,15 @@ export default [
         "handle": "@spacex", "time": "2h", "userImage": "/images/elon.jpeg",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345,
-        "comments": 123, "retuits": 432
+        "liked": true, "likes": 2345, "dislikes":121,"disliked": false,
+        "replies": 123, "retuits": 432
     },
     {
-        "_id": 234, "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "userImage": "/images/hbo.webp",
+        "_id": 234, "topic": "Traffic", "username": "HBO",
+        "handle": "@hbo", "time": "2h", "userImage": "/images/hbo.webp",
         "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345,
-        "comments": 123, "retuits": 432
+        "tuit": "While we wait, If you’re interested in engaging with more of the Targaryen lore as well as ASOIAF in general, but are confused where to start… • Here is a thread on the books set within the TWOIAF that have been published by GRRM, and each of their purposes!",
+        "liked": true, "likes": 2345, "dislikes":991,"disliked": false,
+        "replies": 123, "retuits": 432
     },
 ];
